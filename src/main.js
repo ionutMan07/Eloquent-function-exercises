450fa17f20ec27bfeb19cdd94ceb385f1ca1441b
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-10-04 16:22:39 +0300

    done

--- src/main.js
+++ src/main.js
@@ -11,5 +11,25 @@
 //   else return b;
 // }
 // console.log(min(0, 10));
-
 // console.log(min(0, -10));
+
+//ex.2 solution 1
+
+// let isEven = (x) => {
+//   if (x > 0 && x % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// function isEven(n) {
+//   if (n === 0) return true;
+//   else if (n === 1) return false;
+//   else if (n < 0) return isEven(-n);
+//   else return isEven(n - 2);
+// }
+
+// console.log(isEven(50));
+// console.log(isEven(75));
+// console.log(isEven(-2));
