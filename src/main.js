1446e63165e37a76597ee74ad89710ec2a77ab55
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-10-04 21:38:00 +0300

    done

--- src/main.js
+++ src/main.js
@@ -33,3 +33,47 @@
 // console.log(isEven(50));
 // console.log(isEven(75));
 // console.log(isEven(-2));
+
+// ex3. solution 1
+
+// let countBs = function (string) {
+//   let count = 0;
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] === 'B') {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+
+// let countChar = function (x, y) {
+//   let countC = 0;
+//   for (let i = 0; i < x.length; i++) {
+//     if (x[i] === y) {
+//       countC++;
+//     }
+//   }
+//   return countC;
+// };
+
+// console.log(countBs('BBBBC'));
+// console.log(countChar('kakkerlak', 'k'));
+
+//ex3. solution 2
+
+function countChar(string, ch) {
+  let counted = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) {
+      counted += 1;
+    }
+  }
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, 'B');
+}
+
+console.log(countBs('BBC'));
+console.log(countChar('kakkerlak', 'k'));
