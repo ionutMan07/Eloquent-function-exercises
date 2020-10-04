22456c444db474775a35a47dc849586af7915b72
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-10-04 16:09:34 +0300

    done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-let min = (x, y) => {
-  return Math.min(x, y);
-};
+// ex.1 solution 1
 
-console.log(min(0, 10));
+// let min = (x, y) => {
+//   return Math.min(x, y);
+// };
 
-console.log(min(0, -10));
+//ex.1 solution 2
+
+// function min(a, b) {
+//   if (a < b) return a;
+//   else return b;
+// }
+// console.log(min(0, 10));
+
+// console.log(min(0, -10));
